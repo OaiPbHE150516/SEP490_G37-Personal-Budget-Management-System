bfb160b8365a1aef4e209ee9cf7ce72f8a2cad5b
BaOai.Work <oaipbhe150516@fpt.edu.vn>
2024-01-26 21:00:35 +0700

    update connection string

--- pbms_be/Data/PbmsDbContext.cs
+++ pbms_be/Data/PbmsDbContext.cs
@@ -20,11 +20,25 @@ namespace pbms_be.Data
             // check if running on GCP or local machine
             if (System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
             {
-                connectionString = "Host=/cloudsql/lexical-aileron-410114:asia-southeast1:pbmspostgresql;User Id=postgres;Password=123456789;Database=postgres";
+                var instanceConnectionName = System.Environment.GetEnvironmentVariable("INSTANCE_UNIX_SOCKET");
+                var databaseName = System.Environment.GetEnvironmentVariable("DB_NAME");
+                var userId = System.Environment.GetEnvironmentVariable("DB_USER");
+                var password = System.Environment.GetEnvironmentVariable("DB_PASS");
+                connectionString = $"Host=/cloudsql/{instanceConnectionName};User Id={userId};Password={password};Database={databaseName}";
+                //connectionString = "Host=/cloudsql/lexical-aileron-410114:asia-southeast1:pbmspostgresql;User Id=postgres;Password=123456789;Database=postgres";
             }
             else
             {
-                connectionString = "Host=35.198.233.122;User Id=postgres;Password=123456789;Database=postgres";
+                // get connection string environment variable
+                var env = System.Environment.GetEnvironmentVariable("PBMS_CONNECTION_STRING");
+                // if not set. throw exception and stop the program
+                if (env == null)
+                {
+                    throw new System.Exception("Connection string not set");
+                }
+                connectionString = env;
+                //connectionString = _configuration.GetConnectionString("ConnectionString");
+                //connectionString = "Host=35.198.233.122;User Id=postgres;Password=123456789;Database=postgres";
             }
             optionsBuilder.UseNpgsql(connectionString);
         }
